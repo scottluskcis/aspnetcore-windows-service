407df80713e68e0fb3c4168ad6d1206257ae6b64
scottluskcis <scottlusk.cis@outlook.com>
2019-05-10 21:13:26 -0400

    add TimedhostedService class and make use of ServiceProvider CreateScope

---
+++ HostedServiceExample/ServiceApplication/HostedServices/TimedHostedService.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ServiceApplication.Application;
+
+namespace ServiceApplication.HostedServices
+{
+    internal class TimedHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private Timer _timer;
+
+        public IServiceProvider Services { get; }
+
+        public TimedHostedService(IServiceProvider services, ILogger<TimedHostedService> logger)
+        {
+            Services = services;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Background Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, 
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("Timed Background Service is working.");
+
+            using (var scope = Services.CreateScope())
+            {
+                var scopedProcessingService =
+                    scope.ServiceProvider
+                        .GetRequiredService<IServiceWorker>();
+
+                scopedProcessingService.DoWork();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Background Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
